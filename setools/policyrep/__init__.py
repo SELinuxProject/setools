8d3b4d35a061bc49b56fb4028bee3efe705d8343
Chris PeBenito <cpebenito@tresys.com>
2014-11-01 15:38:31 -0400

    PEP8 fix in SELinuxPolicy.

--- setools/policyrep/__init__.py
+++ setools/policyrep/__init__.py
@@ -70,7 +70,8 @@ class SELinuxPolicy(object):
         try:
             self.policy = qpol.qpol_policy_t(policyfile, 0)
         except OSError as err:
-            raise OSError("Error opening policy file \"{0}\": {1}".format(policyfile,err))
+            raise OSError(
+                "Error opening policy file \"{0}\": {1}".format(policyfile, err))
 
     #
     # Policy components generators
