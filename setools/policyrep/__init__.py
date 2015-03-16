5312074aa52ddf1e75df2b93fb5d587f36243ed6
Chris PeBenito <cpebenito@tresys.com>
2015-03-16 10:47:48 -0400

    Remove unnecessary bool conversion for SELinuxPolicy.mls.

--- setools/policyrep/__init__.py
+++ setools/policyrep/__init__.py
@@ -88,7 +88,7 @@ class SELinuxPolicy(object):
     @property
     def mls(self):
         """(T/F) The policy has MLS enabled."""
-        return bool(mls.enabled(self.policy))
+        return mls.enabled(self.policy)
 
     @property
     def version(self):
