3e6a509ecb0e667891126984caf419c7d2e53d9c
Chris PeBenito <cpebenito@tresys.com>
2015-03-27 13:22:12 -0400

    NodeconQueryTest: fix typo in messages

--- tests/nodeconquery.py
+++ tests/nodeconquery.py
@@ -214,7 +214,7 @@ class NodeconQueryTest(unittest.TestCase):
         nodecons = sorted(n.address for n in q.results())
         self.assertListEqual(["10.1.55.1"], nodecons)
 
-    @unittest.skipIf(sys.version_info < (3, 3), "Requries Python 3.3+.")
+    @unittest.skipIf(sys.version_info < (3, 3), "Requires Python 3.3+.")
     def test_100_v4network_equal(self):
         """Nodecon query with IPv4 equal network"""
         q = NodeconQuery(self.p, net="10.1.100.0/24", net_overlap=False)
@@ -222,7 +222,7 @@ class NodeconQueryTest(unittest.TestCase):
         nodecons = sorted(n.address for n in q.results())
         self.assertListEqual(["10.1.100.0"], nodecons)
 
-    @unittest.skipIf(sys.version_info < (3, 3), "Requries Python 3.3+.")
+    @unittest.skipIf(sys.version_info < (3, 3), "Requires Python 3.3+.")
     def test_101_v4network_overlap(self):
         """Nodecon query with IPv4 network overlap"""
         q = NodeconQuery(self.p, net="10.1.101.128/25", net_overlap=True)
@@ -230,7 +230,7 @@ class NodeconQueryTest(unittest.TestCase):
         nodecons = sorted(n.address for n in q.results())
         self.assertListEqual(["10.1.101.0"], nodecons)
 
-    @unittest.skipIf(sys.version_info < (3, 3), "Requries Python 3.3+.")
+    @unittest.skipIf(sys.version_info < (3, 3), "Requires Python 3.3+.")
     def test_110_v6network_equal(self):
         """Nodecon query with IPv6 equal network"""
         q = NodeconQuery(self.p, net="1100::/16", net_overlap=False)
@@ -238,7 +238,7 @@ class NodeconQueryTest(unittest.TestCase):
         nodecons = sorted(n.address for n in q.results())
         self.assertListEqual(["1100::"], nodecons)
 
-    @unittest.skipIf(sys.version_info < (3, 3), "Requries Python 3.3+.")
+    @unittest.skipIf(sys.version_info < (3, 3), "Requires Python 3.3+.")
     def test_111_v6network_overlap(self):
         """Nodecon query with IPv6 network overlap"""
         q = NodeconQuery(self.p, net="1110:8000::/17", net_overlap=True)
