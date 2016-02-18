403da9aee5a1d67859daeb251de64669a305728f
Chris PeBenito <cpebenito@tresys.com>
2016-02-18 09:46:09 -0500

    Bump version for release.

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ ext_py_mods = [Extension('setools.policyrep._qpol',
                          swig_opts=['-Ilibqpol/include'])]
 
 setup(name='setools',
-      version='4.0.0-alpha3',
+      version='4.0.0-beta',
       description='SELinux Policy tools.',
       author='Tresys Technology, LLC',
       author_email='setools@tresys.com',
