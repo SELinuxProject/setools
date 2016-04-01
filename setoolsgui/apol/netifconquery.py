57d446de1b230a04f65d137747ca014b7ad52b29
Chris PeBenito <cpebenito@tresys.com>
2016-04-01 10:35:36 -0400

    NetifconQueryTab: fix log message on completion

--- setoolsgui/apol/netifconquery.py
+++ setoolsgui/apol/netifconquery.py
@@ -252,7 +252,7 @@ class NetifconQueryTab(SEToolsWidget, QScrollArea):
         self.thread.start()
 
     def update_complete(self, count):
-        self.log.info("{0} initial SID statment(s) found.".format(count))
+        self.log.info("{0} netifcon statment(s) found.".format(count))
 
         # update sizes/location of result displays
         if not self.busy.wasCanceled():
