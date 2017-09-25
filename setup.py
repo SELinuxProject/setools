cc313f9791e1605514b6e2ff36a7a6247ddd0caf
Chris PeBenito <pebenito@ieee.org>
2017-09-24 20:40:29 -0400

    __future__ print functions no longer need importing.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from __future__ import print_function
+
 import glob
 from setuptools import setup
 import distutils.log as log
--- tests/policyrep/selinuxpolicy.py
+++ tests/policyrep/selinuxpolicy.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with SETools.  If not, see <http://www.gnu.org/licenses/>.
 #
-from __future__ import print_function
+
 import copy
 import os
 import sys
