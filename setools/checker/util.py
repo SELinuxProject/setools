27b0f2ef84b774af263e0f6e88404d4b7e39ce1c
Chris PeBenito <pebenito@ieee.org>
2020-08-22 20:43:38 -0400

    checker/util.py: Remove redundant functions.  Add a boolean config function.
    Signed-off-by: Chris PeBenito <pebenito@ieee.org>

--- setools/checker/util.py
+++ setools/checker/util.py
@@ -17,115 +17,14 @@
 # <http://www.gnu.org/licenses/>.
 #
 
-import logging
 
-from ..exception import InvalidCheckValue, InvalidClass, InvalidPermission, InvalidType
-from ..util import validate_perms_any
+def config_bool_value(value):
+    """Convert a boolean configuration value."""
 
+    if isinstance(value, str):
+        if value and value.strip().lower() in ("yes", "true", "1"):
+            return True
 
-def config_list_to_class(policy, config):
-    """
-    Convert a space separated string into a set of object classes.
+        return False
 
-    Parameters:
-    policy      A SELinuxPolicy
-    config      A str with a space-separated set of object classes.
-
-    Return:     Frozenset containing policy objects in the config.
-    """
-    if not config:
-        return frozenset()
-
-    try:
-        tclass = frozenset(policy.lookup_class(c) for c in config.split(" ") if c)
-    except InvalidClass as e:
-        raise InvalidCheckValue("Invalid tclass setting: {}".format(e)) from e
-
-    return tclass
-
-
-def config_list_to_perms(policy, config, tclass=None):
-    """
-    Convert a space separated string into a set of permissions.
-
-    Parameters:
-    policy      A SELinuxPolicy
-    config      A str with a space-separated set of permissions.
-
-    Keyword Parameters:
-    tclass      A container of ObjClass.  If specified, the perms must be valid
-                for at least one of the classes.  If not specified, the permissions
-                must be valid for at least one class in the entire policy.
-
-    Return:     Frozenset containing permission strings.
-    """
-    if not config:
-        return frozenset()
-
-    try:
-        perms = frozenset(i for i in config.split(" ") if i)
-        validate_perms_any(perms, tclass=tclass, policy=policy)
-    except InvalidPermission as e:
-        raise InvalidCheckValue("Invalid perms setting: {}".format(e)) from e
-
-    return perms
-
-
-def config_to_type_or_attr(policy, config):
-    """
-    Convert an option into a type or type attribute.
-
-    Parameters:
-    policy      A SELinuxPolicy
-    config      A str with a types/type attributes.
-
-    Return:     A type or typeattribute object.
-    """
-
-    if not config:
-        return None
-
-    try:
-        return policy.lookup_type_or_attr(config.strip())
-    except InvalidType as e:
-        raise InvalidCheckValue("Invalid type/attribute setting: {}".format(e))
-
-
-def config_list_to_types_or_attrs(log, policy, config, strict=True, expand=False):
-    """
-    Convert a space separated string into a set of types/type attributes.
-
-    Parameters:
-    log         A logging object.
-    policy      A SELinuxPolicy
-    config      A str with a space-separated set of types/type attributes.
-
-    Keyword Parameters:
-    strict      Bool, if True policy lookup errors will be a configuration error.
-                If False, only an INFO level warrning will be issued.
-                Default is True.
-    expand      Bool, if True, attributes will be expanded. Default is False.
-
-    Return:     Frozenset containing policy objects in the config.
-    """
-
-    if not config:
-        return frozenset()
-
-    ret = set()
-    for item in (i for i in config.split(" ") if i):
-        try:
-            obj = policy.lookup_type_or_attr(item)
-            if expand:
-                ret.update(obj.expand())
-            else:
-                ret.add(obj)
-        except InvalidType as e:
-            if strict:
-                log.error("Invalid type/attribute: {}".format(e))
-                log.debug("Traceback:", exc_info=e)
-                raise InvalidCheckValue("Invalid type/attribute setting: {}".format(e)) from e
-
-            log.info("Invalid type/attribute setting: {}".format(e))
-
-    return frozenset(ret)
+    return bool(value)
--- tests/checker/util.py
+++ tests/checker/util.py
@@ -21,170 +21,21 @@ import os
 import logging
 import unittest
 
-from ..policyrep.util import compile_policy
 
 from setools.checker import util
-from setools.exception import InvalidCheckValue
 
 
 class CheckerUtilTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/checker/util.conf")
-        cls.log = logging.getLogger("this_is_a_log_test")
+    def test_config_bool_value(self):
+        """Test config_bool_value"""
+        self.assertTrue(util.config_bool_value(" TrUe "))
+        self.assertTrue((util.config_bool_value(" 1 ")))
+        self.assertTrue((util.config_bool_value(" YeS ")))
+        self.assertFalse((util.config_bool_value(" FalsE ")))
+        self.assertFalse((util.config_bool_value(" 0 ")))
+        self.assertFalse((util.config_bool_value(" No ")))
 
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_config_list_to_class(self):
-        """Test config_list_to_class() success."""
-        expected = set((self.p.lookup_class("infoflow"),
-                        self.p.lookup_class("infoflow2")))
-
-        result = util.config_list_to_class(self.p, " infoflow   infoflow2 ")
-        self.assertIsInstance(result, frozenset)
-        self.assertSetEqual(expected, result)
-
-    def test_config_list_to_class_unset(self):
-        """Test config_list_to_class() with empty input."""
-        result = util.config_list_to_class(self.p, "")
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-        result = util.config_list_to_class(self.p, None)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-    def test_config_list_to_class_error(self):
-        """Test config_list_to_class() with invalid class."""
-        with self.assertRaises(InvalidCheckValue):
-            util.config_list_to_class(self.p, "FAIL")
-
-        with self.assertRaises(InvalidCheckValue):
-            util.config_list_to_class(self.p, "infoflow  FAIL")
-
-    def test_config_list_to_perms_no_class(self):
-        """Test config_list_to_perms() success with no classes set."""
-        expected = set(("hi_w", "null"))
-
-        result = util.config_list_to_perms(self.p, " hi_w   null ", tclass=None)
-        self.assertIsInstance(result, frozenset)
-        self.assertSetEqual(expected, result)
-
-    def test_config_list_to_perms_no_class_unset(self):
-        """Test config_list_to_perms() with empty input with no classes set."""
-        result = util.config_list_to_perms(self.p, "", tclass=None)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-        result = util.config_list_to_perms(self.p, None, tclass=None)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-    def test_config_list_to_perms_no_class_fail(self):
-        """Test config_list_to_perms() failure with no classes set."""
-        with self.assertRaises(InvalidCheckValue):
-            util.config_list_to_perms(self.p, " hi_w   null   invalid_perm ", tclass=None)
-
-    def test_config_list_to_perms_class(self):
-        """Test config_list_to_perms() success with classes set."""
-        classes = set((self.p.lookup_class("infoflow2"),
-                       self.p.lookup_class("infoflow3")))
-        expected = set(("super_r", "null"))
-
-        result = util.config_list_to_perms(self.p, " super_r   null ", tclass=classes)
-        self.assertIsInstance(result, frozenset)
-        self.assertSetEqual(expected, result)
-
-    def test_config_list_to_perms_class_unset(self):
-        """Test config_list_to_perms() with empty input with classes set."""
-        classes = set((self.p.lookup_class("infoflow2"),
-                       self.p.lookup_class("infoflow3")))
-
-        result = util.config_list_to_perms(self.p, "", tclass=classes)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-        result = util.config_list_to_perms(self.p, None, tclass=classes)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-    def test_config_list_to_perms_class_fail(self):
-        """Test config_list_to_perms() failure with classes set."""
-        classes = set((self.p.lookup_class("infoflow2"),
-                       self.p.lookup_class("infoflow3")))
-
-        with self.assertRaises(InvalidCheckValue):
-            # super_none isn't in either class
-            util.config_list_to_perms(self.p, " super_none   null ", tclass=classes)
-
-    def test_config_to_type_or_attr(self):
-        """Test config_to_type_or_attr() success."""
-        expected = self.p.lookup_type("test1")
-        result = util.config_to_type_or_attr(self.p, " test1 ")
-        self.assertEqual(expected, result)
-
-        expected = self.p.lookup_typeattr("test10c")
-        result = util.config_to_type_or_attr(self.p, " test10c ")
-        self.assertEqual(expected, result)
-
-    def test_config_to_type_or_attr_empty(self):
-        """Test config_to_type_or_attr() success."""
-        result = util.config_to_type_or_attr(self.p, "")
-        self.assertIsNone(result)
-
-        result = util.config_to_type_or_attr(self.p, None)
-        self.assertIsNone(result)
-
-    def test_config_to_type_or_attr_fail(self):
-        """Test config_to_type_or_attr() failure."""
-        with self.assertRaises(InvalidCheckValue):
-            util.config_to_type_or_attr(self.p, "FAIL")
-
-    def test_config_list_to_types_or_attrs(self):
-        """Test config_list_to_types_or_attrs() success."""
-        expected = set((self.p.lookup_type("test1"),
-                        self.p.lookup_typeattr("test10c")))
-
-        result = util.config_list_to_types_or_attrs(self.log, self.p, " test1  test10c ")
-        self.assertIsInstance(result, frozenset)
-        self.assertSetEqual(expected, result)
-
-    def test_config_list_to_types_or_attrs_expand(self):
-        """Test config_list_to_types_or_attrs() success and expanded attributes."""
-        expected = set((self.p.lookup_type("test1"),
-                        self.p.lookup_type("test10t3"),
-                        self.p.lookup_type("test10t4"),
-                        self.p.lookup_type("test10t5"),
-                        self.p.lookup_type("test10t7")))
-
-        result = util.config_list_to_types_or_attrs(self.log, self.p, " test1  test10c ",
-                                                    expand=True)
-        self.assertIsInstance(result, frozenset)
-        self.assertSetEqual(expected, result)
-
-    def test_config_list_to_types_or_attrs_unset(self):
-        """Test config_list_to_types_or_attrs() with empty input."""
-        result = util.config_list_to_types_or_attrs(self.log, self.p, "")
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-        result = util.config_list_to_types_or_attrs(self.log, self.p, None)
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
-
-    def test_config_list_to_types_or_attrs_fail_strict(self):
-        """Test config_list_to_types_or_attrs() with strict failure."""
-        with self.assertRaises(InvalidCheckValue):
-            util.config_list_to_types_or_attrs(self.log, self.p, "FAIL", strict=True)
-
-    def test_config_list_to_types_or_attrs_fail_not_strict(self):
-        """Test config_list_to_types_or_attrs() with not strict failure."""
-
-        with self.assertLogs(logger=self.log, level=logging.INFO):
-            result = util.config_list_to_types_or_attrs(self.log, self.p, "FAIL", strict=False)
-
-        self.assertIsInstance(result, frozenset)
-        self.assertEqual(0, len(result))
+        self.assertTrue(util.config_bool_value(True))
+        self.assertFalse((util.config_bool_value(None)))
+        self.assertFalse((util.config_bool_value(False)))
