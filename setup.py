4758cdf803d93274f49cb6445cb2bab527d6549f
Chris PeBenito <pebenito@ieee.org>
2021-03-05 11:19:11 -0500

    setup.py: Update for release 4.4.0.
    Signed-off-by: Chris PeBenito <pebenito@ieee.org>

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ for lang in linguas:
         installed_data.append((join('share/man', lang, 'man1'), glob.glob(join("man", lang, "*.1"))))
 
 setup(name='setools',
-      version='4.4.0-dev',
+      version='4.4.0',
       description='SELinux policy analysis tools.',
       author='Chris PeBenito',
       author_email='pebenito@ieee.org',
