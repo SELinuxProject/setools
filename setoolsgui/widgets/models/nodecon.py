5571037c1e0dd6eacf3c7d73fce01a10c8deb2ff
Chris PeBenito <pebenito@ieee.org>
2023-11-13 10:20:21 -0500

    Add new NodeconQueryTab implementation.
    Signed-off-by: Chris PeBenito <pebenito@ieee.org>

--- setoolsgui/widgets/models/nodecon.py
+++ setoolsgui/widgets/models/nodecon.py
@@ -6,6 +6,8 @@
 from PyQt6 import QtCore
 import setools
 
+from .. import details
+from . import modelroles
 from .table import SEToolsTableModel
 
 __all__ = ("NodeconTable",)
@@ -32,5 +34,33 @@ class NodeconTable(SEToolsTableModel[setools.Nodecon]):
                         return str(rule.network.with_netmask)
                     case 1:
                         return str(rule.context)
+
+            case modelroles.ContextMenuRole:
+                if col == 1:
+                    return details.context_detail_action(rule.context)
+
+            case QtCore.Qt.ItemDataRole.WhatsThisRole:
+                match col:
+                    case 0:
+                        column_whatsthis = \
+                            """
+                            <p>This is the network of the nodecon.</p>
+                            """
+                    case 2:
+                        column_whatsthis = \
+                            """
+                            <p>This is the context of the nodecon.</p>
+                            """
+                    case _:
+                        column_whatsthis = ""
+
+                return \
+                    f"""
+                    <b><p>Table Representation of Network Node Contexts (nodecon)</p></b>
+
+                    <p>Each part of the rule is represented as a column in the table.</p>
+
+                    {column_whatsthis}
+                    """
 
         return super().data(index, role)
