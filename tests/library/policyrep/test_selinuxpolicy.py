16153794a798b8512f0e7ca85bc1af91f72553af
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-08 11:07:39 -0400

    TestSELinuxPolicy: Update tests to pytest fixtures and for Cython implementation.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/policyrep/test_selinuxpolicy.py
+++ tests/library/policyrep/test_selinuxpolicy.py
@@ -4,228 +4,190 @@
 #
 
 import copy
-import os
-import sys
-import unittest
 
-from setools import SELinuxPolicy, HandleUnknown
-from setools.exception import InvalidPolicy
-
-from .util import compile_policy
+import pytest
+import setools
 
 
-class SELinuxPolicyTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/policyrep/selinuxpolicy.conf")
+class TestSELinuxPolicy:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/policyrep/selinuxpolicy.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    @unittest.skip("Retired for the SELinuxPolicyLoadError test suite.")
-    def test_001_open_policy_error(self):
-        """SELinuxPolicy: Invalid policy on open."""
-        # source policies not supported
-        self.assertRaises(InvalidPolicy, SELinuxPolicy, "tests/policyrep/selinuxpolicy-bad.conf")
-
-    def test_002_open_policy_non_existant(self):
+    def test_open_policy_non_existant(self) -> None:
         """SELinuxPolicy: Non existant policy on open."""
-        self.assertRaises(OSError, SELinuxPolicy, "tests/policyrep/DOES_NOT_EXIST")
+        with pytest.raises(OSError):
+            setools.SELinuxPolicy("tests/policyrep/DOES_NOT_EXIST")
 
-    def test_003_deepcopy(self):
+    def test_deepcopy(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: Deep copy"""
-        p = copy.deepcopy(self.p)
-        self.assertIs(self.p, p)
+        p = copy.deepcopy(compiled_policy)
+        assert p is compiled_policy
 
-    def test_010_handle_unknown(self):
+    def test_handle_unknown(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: handle unknown setting."""
-        self.assertEqual(self.p.handle_unknown, HandleUnknown.reject)
+        assert compiled_policy.handle_unknown == setools.HandleUnknown.reject
 
-    def test_011_mls(self):
+    def test_mls(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: MLS status."""
-        self.assertTrue(self.p.mls)
+        assert compiled_policy.mls
 
-    def test_012_version(self):
+    def test_version(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: version."""
-        self.assertTrue(self.p.version)
+        assert compiled_policy.version
 
-    def test_100_allow_count(self):
+    def test_allow_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: allow count"""
-        self.assertEqual(self.p.allow_count, 113)
+        assert compiled_policy.allow_count == 113
 
-    def test_101_auditallow_count(self):
+    def test_auditallow_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: auditallow count"""
-        self.assertEqual(self.p.auditallow_count, 109)
+        assert compiled_policy.auditallow_count == 109
 
-    def test_102_boolean_count(self):
+    def test_boolean_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: Boolean count."""
-        self.assertEqual(self.p.boolean_count, 127)
+        assert compiled_policy.boolean_count == 127
 
-    # def test_103_bounds_count(self):
+    # def test_bounds_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
 
-    def test_104_category_count(self):
+    def test_category_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: category count"""
-        self.assertEqual(self.p.category_count, 17)
+        assert compiled_policy.category_count == 17
 
-    def test_105_class_count(self):
+    def test_class_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: object class count"""
-        self.assertEqual(self.p.class_count, 7)
+        assert compiled_policy.class_count == 7
 
-    def test_106_common_count(self):
+    def test_common_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: common permisison set count"""
-        self.assertEqual(self.p.common_count, 3)
+        assert compiled_policy.common_count == 3
 
-    def test_107_conditional_count(self):
+    def test_conditional_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: conditional (expression) count"""
-        self.assertEqual(self.p.conditional_count, 67)
+        assert compiled_policy.conditional_count == 67
 
-    def test_108_constraint_count(self):
+    def test_constraint_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: standard constraint count"""
-        self.assertEqual(self.p.constraint_count, 19)
+        assert compiled_policy.constraint_count == 19
 
-    # def test_109_default_count(self):
+    # def test_default_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
 
-    def test_110_dontaudit_count(self):
+    def test_dontaudit_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: dontaudit rule count"""
-        self.assertEqual(self.p.dontaudit_count, 107)
+        assert compiled_policy.dontaudit_count == 107
 
-    def test_111_fs_use_count(self):
+    def test_fs_use_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: fs_use_* count"""
-        self.assertEqual(self.p.fs_use_count, 149)
+        assert compiled_policy.fs_use_count == 149
 
-    def test_112_genfscon_count(self):
+    def test_genfscon_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: genfscon count"""
-        self.assertEqual(self.p.genfscon_count, 151)
+        assert compiled_policy.genfscon_count == 151
 
-    def test_113_initial_sid_count(self):
+    def test_initial_sid_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: initial sid count"""
-        self.assertEqual(self.p.initialsids_count, 11)
+        assert compiled_policy.initialsids_count == 11
 
-    def test_114_level_count(self):
+    def test_level_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: MLS level count"""
-        self.assertEqual(self.p.level_count, 13)
+        assert compiled_policy.level_count == 13
 
-    def test_115_mls_constraint_count(self):
+    def test_mls_constraint_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: MLS constraint count"""
-        self.assertEqual(self.p.mlsconstraint_count, 23)
+        assert compiled_policy.mlsconstraint_count == 23
 
-    def test_116_mls_validatetrans_count(self):
+    def test_mls_validatetrans_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: MLS validatetrans count"""
-        self.assertEqual(self.p.mlsvalidatetrans_count, 3)
+        assert compiled_policy.mlsvalidatetrans_count == 3
 
-    def test_117_netifcon_count(self):
+    def test_netifcon_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: netifcon count"""
-        self.assertEqual(self.p.netifcon_count, 167)
+        assert compiled_policy.netifcon_count == 167
 
-    def test_118_neverallow_count(self):
+    def test_neverallow_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: neverallow rule count"""
         # changed after dropping source policy support
-        # self.assertEqual(self.p.neverallow_count, 103)
-        self.assertEqual(self.p.neverallow_count, 0)
+        # assert compiled_policy.neverallow_count == 103)
+        assert compiled_policy.neverallow_count == 0
 
-    def test_119_nodecon_count(self):
+    def test_nodecon_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: nodecon count"""
-        self.assertEqual(self.p.nodecon_count, 173)
+        assert compiled_policy.nodecon_count == 173
 
-    def test_120_permission_count(self):
+    def test_permission_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: permission count"""
-        self.assertEqual(self.p.permission_count, 29)
+        assert compiled_policy.permission_count == 29
 
-    def test_121_permissive_types_count(self):
+    def test_permissive_types_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: permissive types count"""
-        self.assertEqual(self.p.permissives_count, 73)
+        assert compiled_policy.permissives_count == 73
 
-    def test_122_polcap_count(self):
+    def test_polcap_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: policy capability count"""
-        self.assertEqual(self.p.polcap_count, 2)
+        assert compiled_policy.polcap_count == 2
 
-    def test_123_portcon_count(self):
+    def test_portcon_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: portcon count"""
-        self.assertEqual(self.p.portcon_count, 163)
+        assert compiled_policy.portcon_count == 163
 
-    def test_124_range_transition_count(self):
+    def test_range_transition_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: range_transition count"""
-        self.assertEqual(self.p.range_transition_count, 71)
+        assert compiled_policy.range_transition_count == 71
 
-    def test_125_role_count(self):
+    def test_role_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: role count"""
-        self.assertEqual(self.p.role_count, 131)
+        assert compiled_policy.role_count == 131
 
-    # def test_126_role_attribute_count(self):
+    # def test_role_attribute_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
 
-    def test_127_role_allow_count(self):
+    def test_role_allow_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: (role) allow count"""
-        self.assertEqual(self.p.role_allow_count, 83)
+        assert compiled_policy.role_allow_count == 83
 
-    def test_128_role_transition_count(self):
+    def test_role_transition_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: role_transition count"""
-        self.assertEqual(self.p.role_transition_count, 79)
+        assert compiled_policy.role_transition_count == 79
 
-    def test_129_type_count(self):
+    def test_type_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: type count"""
-        self.assertEqual(self.p.type_count, 137)
+        assert compiled_policy.type_count == 137
 
-    def test_130_type_attribute_count(self):
+    def test_type_attribute_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: type attribute count"""
-        self.assertEqual(self.p.type_attribute_count, 157)
+        assert compiled_policy.type_attribute_count == 157
 
-    def test_131_type_change_count(self):
+    def test_type_change_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: type_change rule count"""
-        self.assertEqual(self.p.type_change_count, 89)
+        assert compiled_policy.type_change_count == 89
 
-    def test_132_type_member_count(self):
+    def test_type_member_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: type_member rule count"""
-        self.assertEqual(self.p.type_member_count, 61)
+        assert compiled_policy.type_member_count == 61
 
-    def test_133_type_transition_count(self):
+    def test_type_transition_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: type_transition rule count"""
-        self.assertEqual(self.p.type_transition_count, 97)
+        assert compiled_policy.type_transition_count == 97
 
-    def test_134_user_count(self):
+    def test_user_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: user count"""
-        self.assertEqual(self.p.user_count, 101)
+        assert compiled_policy.user_count == 101
 
-    def test_135_validatetrans_count(self):
+    def test_validatetrans_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: validatetrans count"""
-        self.assertEqual(self.p.validatetrans_count, 5)
+        assert compiled_policy.validatetrans_count == 5
 
-    def test_136_allowxperm_count(self):
+    def test_allowxperm_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: allowxperm rount"""
-        self.assertEqual(self.p.allowxperm_count, 179)
+        assert compiled_policy.allowxperm_count == 179
 
-    def test_137_allowxperm_count(self):
+    def test_auditallowxperm_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: auditallowxperm rount"""
-        self.assertEqual(self.p.auditallowxperm_count, 181)
+        assert compiled_policy.auditallowxperm_count == 181
 
-    def test_138_neverallowxperm_count(self):
+    def test_neverallowxperm_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: neverallowxperm rount"""
         # changed after dropping source policy support
-        # self.assertEqual(self.p.neverallowxperm_count, 191)
-        self.assertEqual(self.p.neverallowxperm_count, 0)
+        # assert compiled_policy.neverallowxperm_count == 191)
+        assert compiled_policy.neverallowxperm_count == 0
 
-    def test_139_allowxperm_count(self):
+    def test_dontauditxperm_count(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """SELinuxPolicy: dontauditxperm rount"""
-        self.assertEqual(self.p.dontauditxperm_count, 193)
-
-
-@unittest.skip("No longer necessary since source policy support was dropped.")
-class SELinuxPolicyLoadError(unittest.TestCase):
-
-    """Test attempted loading of non-compiling policies."""
-
-    def _load_policy(self, policy):
-        self.assertRaises(InvalidPolicy, SELinuxPolicy,
-                          "tests/policyrep/invalid_policies/" + policy)
-
-    def test_nodecon_invalid_range(self):
-        """SELinuxPolicy: invalid nodecon range (category not associated) error."""
-        self._load_policy("nodecon-invalid-range.conf")
-        sys.stderr.write("The \"category can not be associated\" error above is expected.")
-
-    # this is not fixed yet. See issue #72
-    @unittest.expectedFailure
-    def test_user_level_not_in_range(self):
-        """SELinuxPolicy: error for user's default level isn't within the range."""
-        self._load_policy("user-level-not-in-range.conf")
+        assert compiled_policy.dontauditxperm_count == 193
