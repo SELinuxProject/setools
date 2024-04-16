2f6d5b7406c48d89d113fe69d3129ce015ffd885
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-16 16:19:45 -0400

    TestCheckerUtil: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/checker/util.py
+++ tests/library/checker/util.py
@@ -1,41 +1,21 @@
 # Copyright 2020, Microsoft Corporation
 #
-# This file is part of SETools.
+# SPDX-License-Identifier: GPL-2.0-only
 #
-# SETools is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation, either version 2.1 of
-# the License, or (at your option) any later version.
-#
-# SETools is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with SETools.  If not, see
-# <http://www.gnu.org/licenses/>.
-#
-
-import os
-import logging
-import unittest
-
-
 from setools.checker import util
 
 
-class CheckerUtilTest(unittest.TestCase):
+class TestCheckerUtil:
 
     def test_config_bool_value(self):
         """Test config_bool_value"""
-        self.assertTrue(util.config_bool_value(" TrUe "))
-        self.assertTrue((util.config_bool_value(" 1 ")))
-        self.assertTrue((util.config_bool_value(" YeS ")))
-        self.assertFalse((util.config_bool_value(" FalsE ")))
-        self.assertFalse((util.config_bool_value(" 0 ")))
-        self.assertFalse((util.config_bool_value(" No ")))
+        assert util.config_bool_value(" TrUe ")
+        assert (util.config_bool_value(" 1 "))
+        assert (util.config_bool_value(" YeS "))
+        assert not util.config_bool_value(" FalsE ")
+        assert not util.config_bool_value(" 0 ")
+        assert not util.config_bool_value(" No ")
 
-        self.assertTrue(util.config_bool_value(True))
-        self.assertFalse((util.config_bool_value(None)))
-        self.assertFalse((util.config_bool_value(False)))
+        assert util.config_bool_value(True)
+        assert not util.config_bool_value(None)
+        assert not util.config_bool_value(False)
