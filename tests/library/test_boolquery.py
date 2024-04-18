f0592c589f00f5749f88079cdc45130637e471a3
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:18:49 -0400

    TestBoolQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_boolquery.py
+++ tests/library/test_boolquery.py
@@ -2,51 +2,40 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import BoolQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class BoolQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/boolquery.conf")
+class TestBoolQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/boolquery.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Boolean query with no criteria."""
         # query with no parameters gets all Booleans.
-        allbools = sorted(str(b) for b in self.p.bools())
+        allbools = sorted(str(b) for b in compiled_policy.bools())
 
-        q = BoolQuery(self.p)
+        q = setools.BoolQuery(compiled_policy)
         qbools = sorted(str(b) for b in q.results())
 
-        self.assertListEqual(allbools, qbools)
+        assert allbools == qbools
 
-    def test_001_name_exact(self):
+    def test_name_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Boolean query with exact match"""
-        q = BoolQuery(self.p, name="test1")
+        q = setools.BoolQuery(compiled_policy, name="test1")
 
         bools = sorted(str(b) for b in q.results())
-        self.assertListEqual(["test1"], bools)
+        assert ["test1"] == bools
 
-    def test_002_name_regex(self):
+    def test_name_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Boolean query with regex match."""
-        q = BoolQuery(self.p, name="test2(a|b)", name_regex=True)
+        q = setools.BoolQuery(compiled_policy, name="test2(a|b)", name_regex=True)
 
         bools = sorted(str(b) for b in q.results())
-        self.assertListEqual(["test2a", "test2b"], bools)
+        assert ["test2a", "test2b"] == bools
 
-    def test_010_default(self):
+    def test_default(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Boolean query with default state match."""
-        q = BoolQuery(self.p, default=False)
+        q = setools.BoolQuery(compiled_policy, default=False)
 
         bools = sorted(str(b) for b in q.results())
-        self.assertListEqual(["test10a", "test10b"], bools)
+        assert ["test10a", "test10b"] == bools
