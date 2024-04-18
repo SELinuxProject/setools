a25f9f2d4b5cf1dce368b0c02f8f998dcd2bbe61
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:22:44 -0400

    TestDefaultQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_defaultquery.py
+++ tests/library/test_defaultquery.py
@@ -2,90 +2,78 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import DefaultQuery, DefaultRuletype, DefaultValue
-from setools.exception import InvalidClass
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class DefaultQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/defaultquery.conf")
+class TestDefaultQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/defaultquery.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: no criteria."""
         # query with no parameters gets all defaults
-        alldefaults = sorted(self.p.defaults())
+        alldefaults = sorted(compiled_policy.defaults())
 
-        q = DefaultQuery(self.p)
+        q = setools.DefaultQuery(compiled_policy)
         qdefaults = sorted(q.results())
 
-        self.assertListEqual(alldefaults, qdefaults)
+        assert alldefaults == qdefaults
 
-    def test_001_ruletype(self):
+    def test_ruletype(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: ruletype criterion."""
-        q = DefaultQuery(self.p, ruletype=["default_user"])
+        q = setools.DefaultQuery(compiled_policy, ruletype=["default_user"])
         defaults = list(q.results())
-        self.assertEqual(1, len(defaults))
+        assert 1 == len(defaults)
 
         d = defaults[0]
-        self.assertEqual(DefaultRuletype.default_user, d.ruletype)
-        self.assertEqual("infoflow", d.tclass)
-        self.assertEqual(DefaultValue.target, d.default)
+        assert setools.DefaultRuletype.default_user == d.ruletype
+        assert "infoflow" == d.tclass
+        assert setools.DefaultValue.target == d.default
 
-    def test_010_class_list(self):
+    def test_class_list(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: object class list match."""
-        q = DefaultQuery(self.p, tclass=["infoflow3", "infoflow4"])
+        q = setools.DefaultQuery(compiled_policy, tclass=["infoflow3", "infoflow4"])
 
         defaults = sorted(d.tclass for d in q.results())
-        self.assertListEqual(["infoflow3", "infoflow4"], defaults)
+        assert ["infoflow3", "infoflow4"] == defaults
 
-    def test_011_class_regex(self):
+    def test_class_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: object class regex match."""
-        q = DefaultQuery(self.p, tclass="infoflow(3|5)", tclass_regex=True)
+        q = setools.DefaultQuery(compiled_policy, tclass="infoflow(3|5)", tclass_regex=True)
 
         defaults = sorted(c.tclass for c in q.results())
-        self.assertListEqual(["infoflow3", "infoflow5"], defaults)
+        assert ["infoflow3", "infoflow5"] == defaults
 
-    def test_020_default(self):
+    def test_default(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: default setting."""
-        q = DefaultQuery(self.p, default="source")
+        q = setools.DefaultQuery(compiled_policy, default="source")
 
         defaults = sorted(c.tclass for c in q.results())
-        self.assertListEqual(["infoflow", "infoflow3"], defaults)
+        assert ["infoflow", "infoflow3"] == defaults
 
-    def test_030_default_range(self):
+    def test_default_range(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: default_range setting."""
-        q = DefaultQuery(self.p, default_range="high")
+        q = setools.DefaultQuery(compiled_policy, default_range="high")
 
         defaults = sorted(c.tclass for c in q.results())
-        self.assertListEqual(["infoflow7"], defaults)
+        assert ["infoflow7"] == defaults
 
-    def test_900_invalid_ruletype(self):
+    def test_invalid_ruletype(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: invalid ruletype"""
-        with self.assertRaises(KeyError):
-            q = DefaultQuery(self.p, ruletype=["INVALID"])
+        with pytest.raises(KeyError):
+            q = setools.DefaultQuery(compiled_policy, ruletype=["INVALID"])
 
-    def test_901_invalid_class(self):
+    def test_invalid_class(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: invalid object class"""
-        with self.assertRaises(InvalidClass):
-            q = DefaultQuery(self.p, tclass=["INVALID"])
+        with pytest.raises(setools.exception.InvalidClass):
+            q = setools.DefaultQuery(compiled_policy, tclass=["INVALID"])
 
-    def test_902_invalid_default_value(self):
+    def test_invalid_default_value(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: invalid default value"""
-        with self.assertRaises(KeyError):
-            q = DefaultQuery(self.p, default="INVALID")
+        with pytest.raises(KeyError):
+            q = setools.DefaultQuery(compiled_policy, default="INVALID")
 
-    def test_903_invalid_default_range(self):
+    def test_invalid_default_range(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Default query: invalid default range"""
-        with self.assertRaises(KeyError):
-            q = DefaultQuery(self.p, default_range="INVALID")
+        with pytest.raises(KeyError):
+            q = setools.DefaultQuery(compiled_policy, default_range="INVALID")
