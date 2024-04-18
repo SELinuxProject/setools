5a4922227a72cc0e92dc9d673bfb7f02a70e3bb8
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:36:31 -0400

    TestNetifconQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_netifconquery.py
+++ tests/library/test_netifconquery.py
@@ -2,209 +2,212 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import NetifconQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class NetifconQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/netifconquery.conf")
+class TestNetifconQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/netifconquery.conf")
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with no criteria"""
         # query with no parameters gets all netifs.
-        netifs = sorted(self.p.netifcons())
+        netifs = sorted(compiled_policy.netifcons())
 
-        q = NetifconQuery(self.p)
+        q = setools.NetifconQuery(compiled_policy)
         q_netifs = sorted(q.results())
 
-        self.assertListEqual(netifs, q_netifs)
+        assert netifs == q_netifs
 
-    def test_001_name_exact(self):
+    def test_name_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with exact match"""
-        q = NetifconQuery(self.p, name="test1", name_regex=False)
+        q = setools.NetifconQuery(compiled_policy, name="test1", name_regex=False)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test1"], netifs)
+        assert ["test1"] == netifs
 
-    def test_002_name_regex(self):
+    def test_name_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with regex match"""
-        q = NetifconQuery(self.p, name="test2(a|b)", name_regex=True)
+        q = setools.NetifconQuery(compiled_policy, name="test2(a|b)", name_regex=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test2a", "test2b"], netifs)
+        assert ["test2a", "test2b"] == netifs
 
-    def test_010_user_exact(self):
+    def test_user_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context user exact match"""
-        q = NetifconQuery(self.p, user="user10", user_regex=False)
+        q = setools.NetifconQuery(compiled_policy, user="user10", user_regex=False)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test10"], netifs)
+        assert ["test10"] == netifs
 
-    def test_011_user_regex(self):
+    def test_user_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context user regex match"""
-        q = NetifconQuery(self.p, user="user11(a|b)", user_regex=True)
+        q = setools.NetifconQuery(compiled_policy, user="user11(a|b)", user_regex=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test11a", "test11b"], netifs)
+        assert ["test11a", "test11b"] == netifs
 
-    def test_020_role_exact(self):
+    def test_role_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context role exact match"""
-        q = NetifconQuery(self.p, role="role20_r", role_regex=False)
+        q = setools.NetifconQuery(compiled_policy, role="role20_r", role_regex=False)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test20"], netifs)
+        assert ["test20"] == netifs
 
-    def test_021_role_regex(self):
+    def test_role_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context role regex match"""
-        q = NetifconQuery(self.p, role="role21(a|c)_r", role_regex=True)
+        q = setools.NetifconQuery(compiled_policy, role="role21(a|c)_r", role_regex=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test21a", "test21c"], netifs)
+        assert ["test21a", "test21c"] == netifs
 
-    def test_030_type_exact(self):
+    def test_type_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context type exact match"""
-        q = NetifconQuery(self.p, type_="type30", type_regex=False)
+        q = setools.NetifconQuery(compiled_policy, type_="type30", type_regex=False)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test30"], netifs)
+        assert ["test30"] == netifs
 
-    def test_031_type_regex(self):
+    def test_type_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context type regex match"""
-        q = NetifconQuery(self.p, type_="type31(b|c)", type_regex=True)
+        q = setools.NetifconQuery(compiled_policy, type_="type31(b|c)", type_regex=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test31b", "test31c"], netifs)
+        assert ["test31b", "test31c"] == netifs
 
-    def test_040_range_exact(self):
+    def test_range_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range exact match"""
-        q = NetifconQuery(self.p, range_="s0:c1 - s0:c0.c4")
+        q = setools.NetifconQuery(compiled_policy, range_="s0:c1 - s0:c0.c4")
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test40"], netifs)
+        assert ["test40"] == netifs
 
-    def test_041_range_overlap1(self):
+    def test_range_overlap1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range overlap match (equal)"""
-        q = NetifconQuery(self.p, range_="s1:c1 - s1:c0.c4", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s1:c1 - s1:c0.c4", range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test41"], netifs)
+        assert ["test41"] == netifs
 
-    def test_041_range_overlap2(self):
+    def test_range_overlap2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range overlap match (subset)"""
-        q = NetifconQuery(self.p, range_="s1:c1,c2 - s1:c0.c3", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c3",
+                                  range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test41"], netifs)
+        assert ["test41"] == netifs
 
-    def test_041_range_overlap3(self):
+    def test_range_overlap3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range overlap match (superset)"""
-        q = NetifconQuery(self.p, range_="s1 - s1:c0.c4", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s1 - s1:c0.c4", range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test41"], netifs)
+        assert ["test41"] == netifs
 
-    def test_041_range_overlap4(self):
+    def test_range_overlap4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range overlap match (overlap low level)"""
-        q = NetifconQuery(self.p, range_="s1 - s1:c1,c2", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s1 - s1:c1,c2", range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test41"], netifs)
+        assert ["test41"] == netifs
 
-    def test_041_range_overlap5(self):
+    def test_range_overlap5(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range overlap match (overlap high level)"""
-        q = NetifconQuery(self.p, range_="s1:c1,c2 - s1:c0.c4", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c4",
+                                  range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test41"], netifs)
+        assert ["test41"] == netifs
 
-    def test_042_range_subset1(self):
+    def test_range_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range subset match"""
-        q = NetifconQuery(self.p, range_="s2:c1,c2 - s2:c0.c3", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s2:c1,c2 - s2:c0.c3",
+                                  range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test42"], netifs)
+        assert ["test42"] == netifs
 
-    def test_042_range_subset2(self):
+    def test_range_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range subset match (equal)"""
-        q = NetifconQuery(self.p, range_="s2:c1 - s2:c1.c3", range_overlap=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s2:c1 - s2:c1.c3", range_overlap=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test42"], netifs)
+        assert ["test42"] == netifs
 
-    def test_043_range_superset1(self):
+    def test_range_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range superset match"""
-        q = NetifconQuery(self.p, range_="s3 - s3:c0.c4", range_superset=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s3 - s3:c0.c4", range_superset=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test43"], netifs)
+        assert ["test43"] == netifs
 
-    def test_043_range_superset2(self):
+    def test_range_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range superset match (equal)"""
-        q = NetifconQuery(self.p, range_="s3:c1 - s3:c1.c3", range_superset=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s3:c1 - s3:c1.c3", range_superset=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test43"], netifs)
+        assert ["test43"] == netifs
 
-    def test_044_range_proper_subset1(self):
+    def test_range_proper_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper subset match"""
-        q = NetifconQuery(self.p, range_="s4:c1,c2", range_subset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s4:c1,c2", range_subset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test44"], netifs)
+        assert ["test44"] == netifs
 
-    def test_044_range_proper_subset2(self):
+    def test_range_proper_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper subset match (equal)"""
-        q = NetifconQuery(self.p, range_="s4:c1 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s4:c1 - s4:c1.c3", range_subset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual([], netifs)
+        assert [] == netifs
 
-    def test_044_range_proper_subset3(self):
+    def test_range_proper_subset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper subset match (equal low only)"""
-        q = NetifconQuery(self.p, range_="s4:c1 - s4:c1.c2", range_subset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s4:c1 - s4:c1.c2", range_subset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test44"], netifs)
+        assert ["test44"] == netifs
 
-    def test_044_range_proper_subset4(self):
+    def test_range_proper_subset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper subset match (equal high only)"""
-        q = NetifconQuery(self.p,
-                          range_="s4:c1,c2 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s4:c1,c2 - s4:c1.c3", range_subset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test44"], netifs)
+        assert ["test44"] == netifs
 
-    def test_045_range_proper_superset1(self):
+    def test_range_proper_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper superset match"""
-        q = NetifconQuery(self.p, range_="s5 - s5:c0.c4", range_superset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s5 - s5:c0.c4", range_superset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test45"], netifs)
+        assert ["test45"] == netifs
 
-    def test_045_range_proper_superset2(self):
+    def test_range_proper_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper superset match (equal)"""
-        q = NetifconQuery(self.p, range_="s5:c1 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s5:c1 - s5:c1.c3", range_superset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual([], netifs)
+        assert [] == netifs
 
-    def test_045_range_proper_superset3(self):
+    def test_range_proper_superset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper superset match (equal low)"""
-        q = NetifconQuery(self.p, range_="s5:c1 - s5:c1.c4", range_superset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s5:c1 - s5:c1.c4", range_superset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test45"], netifs)
+        assert ["test45"] == netifs
 
-    def test_045_range_proper_superset4(self):
+    def test_range_proper_superset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Netifcon query with context range proper superset match (equal high)"""
-        q = NetifconQuery(self.p, range_="s5 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.NetifconQuery(compiled_policy, range_="s5 - s5:c1.c3", range_superset=True,
+                                  range_proper=True)
 
         netifs = sorted(s.netif for s in q.results())
-        self.assertListEqual(["test45"], netifs)
+        assert ["test45"] == netifs
