88bf8face5a26e0f44b22e5ab09f7d6582693d3c
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:19:40 -0400

    TestCategoryQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_categoryquery.py
+++ tests/library/test_categoryquery.py
@@ -2,58 +2,46 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import CategoryQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class CategoryQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/categoryquery.conf")
+class TestCategoryQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/categoryquery.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """MLS category query with no criteria."""
         # query with no parameters gets all categories.
-        allcats = sorted(str(c) for c in self.p.categories())
+        allcats = sorted(str(c) for c in compiled_policy.categories())
 
-        q = CategoryQuery(self.p)
+        q = setools.CategoryQuery(compiled_policy)
         qcats = sorted(str(c) for c in q.results())
+        assert allcats == qcats
 
-        self.assertListEqual(allcats, qcats)
-
-    def test_001_name_exact(self):
+    def test_name_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """MLS category query with exact name match."""
-        q = CategoryQuery(self.p, name="test1")
+        q = setools.CategoryQuery(compiled_policy, name="test1")
 
         cats = sorted(str(c) for c in q.results())
-        self.assertListEqual(["test1"], cats)
+        assert ["test1"] == cats
 
-    def test_002_name_regex(self):
+    def test_name_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """MLS category query with regex name match."""
-        q = CategoryQuery(self.p, name="test2(a|b)", name_regex=True)
+        q = setools.CategoryQuery(compiled_policy, name="test2(a|b)", name_regex=True)
 
         cats = sorted(str(c) for c in q.results())
-        self.assertListEqual(["test2a", "test2b"], cats)
+        assert ["test2a", "test2b"] == cats
 
-    def test_010_alias_exact(self):
+    def test_alias_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """MLS category query with exact alias match."""
-        q = CategoryQuery(self.p, alias="test10a")
+        q = setools.CategoryQuery(compiled_policy, alias="test10a")
 
         cats = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test10c1"], cats)
+        assert ["test10c1"] == cats
 
-    def test_011_alias_regex(self):
+    def test_alias_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """MLS category query with regex alias match."""
-        q = CategoryQuery(self.p, alias="test11(a|b)", alias_regex=True)
+        q = setools.CategoryQuery(compiled_policy, alias="test11(a|b)", alias_regex=True)
 
         cats = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test11c1", "test11c2"], cats)
+        assert ["test11c1", "test11c2"] == cats
