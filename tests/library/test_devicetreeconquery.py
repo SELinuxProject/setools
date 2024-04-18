d81d7ae9c18af2da641570cd34795b795d7e7ef3
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:23:26 -0400

    TestDevicetreeconQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_devicetreeconquery.py
+++ tests/library/test_devicetreeconquery.py
@@ -2,205 +2,202 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import DevicetreeconQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class DevicetreeconQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/devicetreeconquery.conf", xen=True)
+class TestDevicetreeconQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/devicetreeconquery.conf", xen=True)
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with no criteria"""
         # query with no parameters gets all PCI paths.
-        rules = sorted(self.p.devicetreecons())
+        rules = sorted(compiled_policy.devicetreecons())
 
-        q = DevicetreeconQuery(self.p)
+        q = setools.DevicetreeconQuery(compiled_policy)
         q_rules = sorted(q.results())
 
-        self.assertListEqual(rules, q_rules)
+        assert rules == q_rules
 
-    def test_010_user_exact(self):
+    def test_user_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context user exact match"""
-        q = DevicetreeconQuery(self.p, user="user10", user_regex=False)
+        q = setools.DevicetreeconQuery(compiled_policy, user="user10", user_regex=False)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree10")], path)
+        assert [("/dev/tree10")] == path
 
-    def test_011_user_regex(self):
+    def test_user_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context user regex match"""
-        q = DevicetreeconQuery(self.p, user="user11(a|b)", user_regex=True)
+        q = setools.DevicetreeconQuery(compiled_policy, user="user11(a|b)", user_regex=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree11"), ("/dev/tree11000")], path)
+        assert [("/dev/tree11"), ("/dev/tree11000")] == path
 
-    def test_020_role_exact(self):
+    def test_role_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context role exact match"""
-        q = DevicetreeconQuery(self.p, role="role20_r", role_regex=False)
+        q = setools.DevicetreeconQuery(compiled_policy, role="role20_r", role_regex=False)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree20")], path)
+        assert [("/dev/tree20")] == path
 
-    def test_021_role_regex(self):
+    def test_role_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context role regex match"""
-        q = DevicetreeconQuery(self.p, role="role21(a|c)_r", role_regex=True)
+        q = setools.DevicetreeconQuery(compiled_policy, role="role21(a|c)_r", role_regex=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree21"), ("/dev/tree21001")], path)
+        assert [("/dev/tree21"), ("/dev/tree21001")] == path
 
-    def test_030_type_exact(self):
+    def test_type_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context type exact match"""
-        q = DevicetreeconQuery(self.p, type_="type30", type_regex=False)
+        q = setools.DevicetreeconQuery(compiled_policy, type_="type30", type_regex=False)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree30")], path)
+        assert [("/dev/tree30")] == path
 
-    def test_031_type_regex(self):
+    def test_type_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context type regex match"""
-        q = DevicetreeconQuery(self.p, type_="type31(b|c)", type_regex=True)
+        q = setools.DevicetreeconQuery(compiled_policy, type_="type31(b|c)", type_regex=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree31000"), ("/dev/tree31001")], path)
+        assert [("/dev/tree31000"), ("/dev/tree31001")] == path
 
-    def test_040_range_exact(self):
+    def test_range_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range exact match"""
-        q = DevicetreeconQuery(self.p, range_="s0:c1 - s0:c0.c4")
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s0:c1 - s0:c0.c4")
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree40")], path)
+        assert [("/dev/tree40")] == path
 
-    def test_041_range_overlap1(self):
+    def test_range_overlap1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range overlap match (equal)"""
-        q = DevicetreeconQuery(self.p, range_="s1:c1 - s1:c0.c4", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s1:c1 - s1:c0.c4",
+                                       range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree41")], path)
+        assert [("/dev/tree41")] == path
 
-    def test_041_range_overlap2(self):
+    def test_range_overlap2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range overlap match (subset)"""
-        q = DevicetreeconQuery(self.p, range_="s1:c1,c2 - s1:c0.c3", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c3",
+                                       range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree41")], path)
+        assert [("/dev/tree41")] == path
 
-    def test_041_range_overlap3(self):
+    def test_range_overlap3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range overlap match (superset)"""
-        q = DevicetreeconQuery(self.p, range_="s1 - s1:c0.c4", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s1 - s1:c0.c4", range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree41")], path)
+        assert [("/dev/tree41")] == path
 
-    def test_041_range_overlap4(self):
+    def test_range_overlap4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range overlap match (overlap low level)"""
-        q = DevicetreeconQuery(self.p, range_="s1 - s1:c1,c2", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s1 - s1:c1,c2", range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree41")], path)
+        assert [("/dev/tree41")] == path
 
-    def test_041_range_overlap5(self):
+    def test_range_overlap5(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range overlap match (overlap high level)"""
-        q = DevicetreeconQuery(self.p, range_="s1:c1,c2 - s1:c0.c4", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c4",
+                                       range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree41")], path)
+        assert [("/dev/tree41")] == path
 
-    def test_042_range_subset1(self):
+    def test_range_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range subset match"""
-        q = DevicetreeconQuery(self.p, range_="s2:c1,c2 - s2:c0.c3", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s2:c1,c2 - s2:c0.c3",
+                                       range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree42")], path)
+        assert [("/dev/tree42")] == path
 
-    def test_042_range_subset2(self):
+    def test_range_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range subset match (equal)"""
-        q = DevicetreeconQuery(self.p, range_="s2:c1 - s2:c1.c3", range_overlap=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s2:c1 - s2:c1.c3",
+                                       range_overlap=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree42")], path)
+        assert [("/dev/tree42")] == path
 
-    def test_043_range_superset1(self):
+    def test_range_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range superset match"""
-        q = DevicetreeconQuery(self.p, range_="s3 - s3:c0.c4", range_superset=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s3 - s3:c0.c4",
+                                       range_superset=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree43")], path)
+        assert [("/dev/tree43")] == path
 
-    def test_043_range_superset2(self):
+    def test_range_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range superset match (equal)"""
-        q = DevicetreeconQuery(self.p, range_="s3:c1 - s3:c1.c3", range_superset=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s3:c1 - s3:c1.c3",
+                                       range_superset=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree43")], path)
+        assert [("/dev/tree43")] == path
 
-    def test_044_range_proper_subset1(self):
+    def test_range_proper_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper subset match"""
-        q = DevicetreeconQuery(self.p, range_="s4:c1,c2", range_subset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s4:c1,c2", range_subset=True,
+                                       range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree44")], path)
+        assert [("/dev/tree44")] == path
 
-    def test_044_range_proper_subset2(self):
+    def test_range_proper_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper subset match (equal)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s4:c1 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s4:c1 - s4:c1.c3",
+                                       range_subset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([], path)
+        assert [] == path
 
-    def test_044_range_proper_subset3(self):
+    def test_range_proper_subset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper subset match (equal low only)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s4:c1 - s4:c1.c2", range_subset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s4:c1 - s4:c1.c2",
+                                       range_subset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree44")], path)
+        assert [("/dev/tree44")] == path
 
-    def test_044_range_proper_subset4(self):
+    def test_range_proper_subset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper subset match (equal high only)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s4:c1,c2 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s4:c1,c2 - s4:c1.c3",
+                                       range_subset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree44")], path)
+        assert [("/dev/tree44")] == path
 
-    def test_045_range_proper_superset1(self):
+    def test_range_proper_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper superset match"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s5 - s5:c0.c4", range_superset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s5 - s5:c0.c4",
+                                       range_superset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree45")], path)
+        assert [("/dev/tree45")] == path
 
-    def test_045_range_proper_superset2(self):
+    def test_range_proper_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper superset match (equal)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s5:c1 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s5:c1 - s5:c1.c3",
+                                       range_superset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([], path)
+        assert [] == path
 
-    def test_045_range_proper_superset3(self):
+    def test_range_proper_superset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper superset match (equal low)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s5:c1 - s5:c1.c4", range_superset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s5:c1 - s5:c1.c4",
+                                       range_superset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree45")], path)
+        assert [("/dev/tree45")] == path
 
-    def test_045_range_proper_superset4(self):
+    def test_range_proper_superset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Devicetreecon query with context range proper superset match (equal high)"""
-        q = DevicetreeconQuery(self.p,
-                               range_="s5 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.DevicetreeconQuery(compiled_policy, range_="s5 - s5:c1.c3",
+                                       range_superset=True, range_proper=True)
 
         path = sorted(p.path for p in q.results())
-        self.assertListEqual([("/dev/tree45")], path)
+        assert [("/dev/tree45")] == path
