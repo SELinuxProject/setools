a5cfda01f52cd6dc14b9fd2b933bc74bb798df55
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:43:05 -0400

    TestTypeAttributeQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_typeattrquery.py
+++ tests/library/test_typeattrquery.py
@@ -2,66 +2,55 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import TypeAttributeQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class TypeAttributeQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/typeattrquery.conf")
+class TestTypeAttributeQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/typeattrquery.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with no criteria."""
         # query with no parameters gets all attrs.
-        allattrs = sorted(self.p.typeattributes())
+        allattrs = sorted(compiled_policy.typeattributes())
 
-        q = TypeAttributeQuery(self.p)
+        q = setools.TypeAttributeQuery(compiled_policy)
         qattrs = sorted(q.results())
 
-        self.assertListEqual(allattrs, qattrs)
+        assert allattrs == qattrs
 
-    def test_001_name_exact(self):
+    def test_name_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with exact name match."""
-        q = TypeAttributeQuery(self.p, name="test1")
+        q = setools.TypeAttributeQuery(compiled_policy, name="test1")
 
         attrs = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test1"], attrs)
+        assert ["test1"] == attrs
 
-    def test_002_name_regex(self):
+    def test_name_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with regex name match."""
-        q = TypeAttributeQuery(self.p, name="test2(a|b)", name_regex=True)
+        q = setools.TypeAttributeQuery(compiled_policy, name="test2(a|b)", name_regex=True)
 
         attrs = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test2a", "test2b"], attrs)
+        assert ["test2a", "test2b"] == attrs
 
-    def test_010_type_set_intersect(self):
+    def test_type_set_intersect(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with type set intersection."""
-        q = TypeAttributeQuery(self.p, types=["test10t1", "test10t7"])
+        q = setools.TypeAttributeQuery(compiled_policy, types=["test10t1", "test10t7"])
 
         attrs = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test10a", "test10c"], attrs)
+        assert ["test10a", "test10c"] == attrs
 
-    def test_011_type_set_equality(self):
+    def test_type_set_equality(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with type set equality."""
-        q = TypeAttributeQuery(self.p, types=["test11t1", "test11t2",
-                                              "test11t3", "test11t5"], types_equal=True)
+        q = setools.TypeAttributeQuery(compiled_policy, types=["test11t1", "test11t2",
+                                       "test11t3", "test11t5"], types_equal=True)
 
         attrs = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test11a"], attrs)
+        assert ["test11a"] == attrs
 
-    def test_012_type_set_regex(self):
+    def test_type_set_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Type attribute query with type set regex match."""
-        q = TypeAttributeQuery(self.p, types="test12t(1|2)", types_regex=True)
+        q = setools.TypeAttributeQuery(compiled_policy, types="test12t(1|2)", types_regex=True)
 
         attrs = sorted(str(t) for t in q.results())
-        self.assertListEqual(["test12a", "test12b"], attrs)
+        assert ["test12a", "test12b"] == attrs
