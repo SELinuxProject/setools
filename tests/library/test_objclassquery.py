c112e6a621921743c37f19460e7a6ec3cf273e42
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:37:30 -0400

    TestObjClassQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_objclassquery.py
+++ tests/library/test_objclassquery.py
@@ -2,110 +2,96 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import ObjClassQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class ObjClassQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/objclassquery.conf")
+class TestObjClassQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/objclassquery.conf")
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with no criteria."""
         # query with no parameters gets all types.
-        classes = sorted(self.p.classes())
+        classes = sorted(compiled_policy.classes())
 
-        q = ObjClassQuery(self.p)
+        q = setools.ObjClassQuery(compiled_policy)
         q_classes = sorted(q.results())
 
-        self.assertListEqual(classes, q_classes)
+        assert classes == q_classes
 
-    def test_001_name_exact(self):
+    def test_name_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with exact name match."""
-        q = ObjClassQuery(self.p, name="infoflow")
+        q = setools.ObjClassQuery(compiled_policy, name="infoflow")
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow"], classes)
+        assert ["infoflow"] == classes
 
-    def test_002_name_regex(self):
+    def test_name_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with regex name match."""
-        q = ObjClassQuery(self.p, name="infoflow(2|3)", name_regex=True)
+        q = setools.ObjClassQuery(compiled_policy, name="infoflow(2|3)", name_regex=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow2", "infoflow3"], classes)
+        assert ["infoflow2", "infoflow3"] == classes
 
-    def test_010_common_exact(self):
+    def test_common_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with exact common name match."""
-        q = ObjClassQuery(self.p, common="infoflow")
+        q = setools.ObjClassQuery(compiled_policy, common="infoflow")
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow", "infoflow2",
-                              "infoflow4", "infoflow7"], classes)
+        assert ["infoflow", "infoflow2", "infoflow4", "infoflow7"] == classes
 
-    def test_011_common_regex(self):
+    def test_common_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with regex common name match."""
-        q = ObjClassQuery(self.p, common="com_[ab]", common_regex=True)
+        q = setools.ObjClassQuery(compiled_policy, common="com_[ab]", common_regex=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow5", "infoflow6"], classes)
+        assert ["infoflow5", "infoflow6"] == classes
 
-    def test_020_perm_indirect_intersect(self):
+    def test_perm_indirect_intersect(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with indirect, intersect permission name patch."""
-        q = ObjClassQuery(
-            self.p, perms=set(["send"]), perms_indirect=True, perms_equal=False)
+        q = setools.ObjClassQuery(
+            compiled_policy, perms=set(["send"]), perms_indirect=True, perms_equal=False)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow6"], classes)
+        assert ["infoflow6"] == classes
 
-    def test_021_perm_direct_intersect(self):
+    def test_perm_direct_intersect(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with direct, intersect permission name patch."""
-        q = ObjClassQuery(
-            self.p, perms=set(["super_r"]), perms_indirect=False, perms_equal=False)
+        q = setools.ObjClassQuery(
+            compiled_policy, perms=set(["super_r"]), perms_indirect=False, perms_equal=False)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow2", "infoflow4", "infoflow8"], classes)
+        assert ["infoflow2", "infoflow4", "infoflow8"] == classes
 
-    def test_022_perm_indirect_equal(self):
+    def test_perm_indirect_equal(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with indirect, equal permission name patch."""
-        q = ObjClassQuery(self.p, perms=set(
+        q = setools.ObjClassQuery(compiled_policy, perms=set(
             ["low_w", "med_w", "hi_w", "low_r", "med_r", "hi_r", "unmapped"]),
             perms_indirect=True, perms_equal=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow7"], classes)
+        assert ["infoflow7"] == classes
 
-    def test_023_perm_direct_equal(self):
+    def test_perm_direct_equal(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with direct, equal permission name patch."""
-        q = ObjClassQuery(self.p, perms=set(
+        q = setools.ObjClassQuery(compiled_policy, perms=set(
             ["super_r", "super_w"]), perms_indirect=False, perms_equal=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow2", "infoflow8"], classes)
+        assert ["infoflow2", "infoflow8"] == classes
 
-    def test_024_perm_indirect_regex(self):
+    def test_perm_indirect_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with indirect, regex permission name patch."""
-        q = ObjClassQuery(
-            self.p, perms="(send|setattr)", perms_indirect=True, perms_regex=True)
+        q = setools.ObjClassQuery(
+            compiled_policy, perms="(send|setattr)", perms_indirect=True, perms_regex=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow6", "infoflow9"], classes)
+        assert ["infoflow6", "infoflow9"] == classes
 
-    def test_025_perm_direct_regex(self):
+    def test_perm_direct_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Class query with direct, regex permission name patch."""
-        q = ObjClassQuery(
-            self.p, perms="(read|super_r)", perms_indirect=False, perms_regex=True)
+        q = setools.ObjClassQuery(
+            compiled_policy, perms="(read|super_r)", perms_indirect=False, perms_regex=True)
 
         classes = sorted(str(c) for c in q.results())
-        self.assertListEqual(["infoflow10", "infoflow2",
-                              "infoflow4", "infoflow8"],
-                             classes)
+        assert ["infoflow10", "infoflow2", "infoflow4", "infoflow8"] == classes
