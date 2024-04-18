a4f8dffcf10a86a11199c75a6bee69a532e9f0cc
Chris PeBenito <chpebeni@linux.microsoft.com>
2024-04-18 15:39:00 -0400

    TestPirqconQuery: Update tests to pytest fixtures.
    Signed-off-by: Chris PeBenito <chpebeni@linux.microsoft.com>

--- tests/library/test_pirqconquery.py
+++ tests/library/test_pirqconquery.py
@@ -2,198 +2,195 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 #
-import os
-import unittest
-
-from setools import PirqconQuery
-
-from .policyrep.util import compile_policy
+import pytest
+import setools
 
 
-class PirqconQueryTest(unittest.TestCase):
+@pytest.mark.obj_args("tests/library/pirqconquery.conf", xen=True)
+class TestPirqconQuery:
 
-    @classmethod
-    def setUpClass(cls):
-        cls.p = compile_policy("tests/library/pirqconquery.conf", xen=True)
-
-    @classmethod
-    def tearDownClass(cls):
-        os.unlink(cls.p.path)
-
-    def test_000_unset(self):
+    def test_unset(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with no criteria"""
         # query with no parameters gets all PCI irqs.
-        rules = sorted(self.p.pirqcons())
+        rules = sorted(compiled_policy.pirqcons())
 
-        q = PirqconQuery(self.p)
+        q = setools.PirqconQuery(compiled_policy)
         q_rules = sorted(q.results())
 
-        self.assertListEqual(rules, q_rules)
+        assert rules == q_rules
 
-    def test_010_user_exact(self):
+    def test_user_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context user exact match"""
-        q = PirqconQuery(self.p, user="user10", user_regex=False)
+        q = setools.PirqconQuery(compiled_policy, user="user10", user_regex=False)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(10)], irq)
+        assert [(10)] == irq
 
-    def test_011_user_regex(self):
+    def test_user_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context user regex match"""
-        q = PirqconQuery(self.p, user="user11(a|b)", user_regex=True)
+        q = setools.PirqconQuery(compiled_policy, user="user11(a|b)", user_regex=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(11), (11000)], irq)
+        assert [(11), (11000)] == irq
 
-    def test_020_role_exact(self):
+    def test_role_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context role exact match"""
-        q = PirqconQuery(self.p, role="role20_r", role_regex=False)
+        q = setools.PirqconQuery(compiled_policy, role="role20_r", role_regex=False)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(20)], irq)
+        assert [(20)] == irq
 
-    def test_021_role_regex(self):
+    def test_role_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context role regex match"""
-        q = PirqconQuery(self.p, role="role21(a|c)_r", role_regex=True)
+        q = setools.PirqconQuery(compiled_policy, role="role21(a|c)_r", role_regex=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(21), (21001)], irq)
+        assert [(21), (21001)] == irq
 
-    def test_030_type_exact(self):
+    def test_type_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context type exact match"""
-        q = PirqconQuery(self.p, type_="type30", type_regex=False)
+        q = setools.PirqconQuery(compiled_policy, type_="type30", type_regex=False)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(30)], irq)
+        assert [(30)] == irq
 
-    def test_031_type_regex(self):
+    def test_type_regex(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context type regex match"""
-        q = PirqconQuery(self.p, type_="type31(b|c)", type_regex=True)
+        q = setools.PirqconQuery(compiled_policy, type_="type31(b|c)", type_regex=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(31000), (31001)], irq)
+        assert [(31000), (31001)] == irq
 
-    def test_040_range_exact(self):
+    def test_range_exact(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range exact match"""
-        q = PirqconQuery(self.p, range_="s0:c1 - s0:c0.c4")
+        q = setools.PirqconQuery(compiled_policy, range_="s0:c1 - s0:c0.c4")
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(40)], irq)
+        assert [(40)] == irq
 
-    def test_041_range_overlap1(self):
+    def test_range_overlap1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range overlap match (equal)"""
-        q = PirqconQuery(self.p, range_="s1:c1 - s1:c0.c4", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s1:c1 - s1:c0.c4", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(41)], irq)
+        assert [(41)] == irq
 
-    def test_041_range_overlap2(self):
+    def test_range_overlap2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range overlap match (subset)"""
-        q = PirqconQuery(self.p, range_="s1:c1,c2 - s1:c0.c3", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c3", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(41)], irq)
+        assert [(41)] == irq
 
-    def test_041_range_overlap3(self):
+    def test_range_overlap3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range overlap match (superset)"""
-        q = PirqconQuery(self.p, range_="s1 - s1:c0.c4", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s1 - s1:c0.c4", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(41)], irq)
+        assert [(41)] == irq
 
-    def test_041_range_overlap4(self):
+    def test_range_overlap4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range overlap match (overlap low level)"""
-        q = PirqconQuery(self.p, range_="s1 - s1:c1,c2", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s1 - s1:c1,c2", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(41)], irq)
+        assert [(41)] == irq
 
-    def test_041_range_overlap5(self):
+    def test_range_overlap5(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range overlap match (overlap high level)"""
-        q = PirqconQuery(self.p, range_="s1:c1,c2 - s1:c0.c4", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s1:c1,c2 - s1:c0.c4", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(41)], irq)
+        assert [(41)] == irq
 
-    def test_042_range_subset1(self):
+    def test_range_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range subset match"""
-        q = PirqconQuery(self.p, range_="s2:c1,c2 - s2:c0.c3", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s2:c1,c2 - s2:c0.c3", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(42)], irq)
+        assert [(42)] == irq
 
-    def test_042_range_subset2(self):
+    def test_range_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range subset match (equal)"""
-        q = PirqconQuery(self.p, range_="s2:c1 - s2:c1.c3", range_overlap=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s2:c1 - s2:c1.c3", range_overlap=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(42)], irq)
+        assert [(42)] == irq
 
-    def test_043_range_superset1(self):
+    def test_range_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range superset match"""
-        q = PirqconQuery(self.p, range_="s3 - s3:c0.c4", range_superset=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s3 - s3:c0.c4", range_superset=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(43)], irq)
+        assert [(43)] == irq
 
-    def test_043_range_superset2(self):
+    def test_range_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range superset match (equal)"""
-        q = PirqconQuery(self.p, range_="s3:c1 - s3:c1.c3", range_superset=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s3:c1 - s3:c1.c3", range_superset=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(43)], irq)
+        assert [(43)] == irq
 
-    def test_044_range_proper_subset1(self):
+    def test_range_proper_subset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper subset match"""
-        q = PirqconQuery(self.p, range_="s4:c1,c2", range_subset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s4:c1,c2", range_subset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(44)], irq)
+        assert [(44)] == irq
 
-    def test_044_range_proper_subset2(self):
+    def test_range_proper_subset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper subset match (equal)"""
-        q = PirqconQuery(self.p, range_="s4:c1 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s4:c1 - s4:c1.c3", range_subset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([], irq)
+        assert [] == irq
 
-    def test_044_range_proper_subset3(self):
+    def test_range_proper_subset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper subset match (equal low only)"""
-        q = PirqconQuery(self.p, range_="s4:c1 - s4:c1.c2", range_subset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s4:c1 - s4:c1.c2", range_subset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(44)], irq)
+        assert [(44)] == irq
 
-    def test_044_range_proper_subset4(self):
+    def test_range_proper_subset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper subset match (equal high only)"""
-        q = PirqconQuery(self.p, range_="s4:c1,c2 - s4:c1.c3", range_subset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s4:c1,c2 - s4:c1.c3", range_subset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(44)], irq)
+        assert [(44)] == irq
 
-    def test_045_range_proper_superset1(self):
+    def test_range_proper_superset1(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper superset match"""
-        q = PirqconQuery(self.p, range_="s5 - s5:c0.c4", range_superset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s5 - s5:c0.c4", range_superset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(45)], irq)
+        assert [(45)] == irq
 
-    def test_045_range_proper_superset2(self):
+    def test_range_proper_superset2(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper superset match (equal)"""
-        q = PirqconQuery(self.p, range_="s5:c1 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s5:c1 - s5:c1.c3", range_superset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([], irq)
+        assert [] == irq
 
-    def test_045_range_proper_superset3(self):
+    def test_range_proper_superset3(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper superset match (equal low)"""
-        q = PirqconQuery(self.p, range_="s5:c1 - s5:c1.c4", range_superset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s5:c1 - s5:c1.c4", range_superset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(45)], irq)
+        assert [(45)] == irq
 
-    def test_045_range_proper_superset4(self):
+    def test_range_proper_superset4(self, compiled_policy: setools.SELinuxPolicy) -> None:
         """Pirqcon query with context range proper superset match (equal high)"""
-        q = PirqconQuery(self.p, range_="s5 - s5:c1.c3", range_superset=True, range_proper=True)
+        q = setools.PirqconQuery(compiled_policy, range_="s5 - s5:c1.c3", range_superset=True,
+                                 range_proper=True)
 
         irq = sorted(p.irq for p in q.results())
-        self.assertListEqual([(45)], irq)
+        assert [(45)] == irq
